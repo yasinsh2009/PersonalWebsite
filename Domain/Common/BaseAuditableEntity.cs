7cbbc24b8632e50eeb357af13fd1778bc7d425c9
Yasin Shahmohammadi <yasinsh.dev@gmail.com>
2025-11-02 00:14:42 +0330

    feat : add tenant entity

---
+++ Domain/Common/BaseAuditableEntity.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Common
+{
+    public abstract class BaseAuditableEntity : BaseEntity
+    {
+        public DateTimeOffset CreatedAt { get; set; }
+
+        public string? CreatedBy { get; set; }
+
+        public DateTimeOffset LastModifiedAt { get; set; }
+
+        public string? LastModifiedBy { get; set; }
+    }
+}
---
+++ Domain/Tenant/Tenant.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Common;
+
+namespace Domain.Tenant
+{
+    public class Tenant : BaseAuditableEntity
+    {
+        public Profile Profile { get; set; }
+
+        public ICollection<Project> Projects { get; set; }
+
+        public ICollection<Blog> Blogs { get; set; }
+    }
+}
